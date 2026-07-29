ca0fef262cc9ecdeac91aa90c950c5579637c616
DESKTOP-6SJKUT5\user <qoo728qoo@gmail.com>
2026-07-29 15:40:56 +0900

    PCソフトエンティティクラスの設計を複合主キー設計からサロゲートキー設計に変更

--- src/main/java/com/example/pc_management_app/entity/PcSoft.java
+++ src/main/java/com/example/pc_management_app/entity/PcSoft.java
@@ -1,17 +1,30 @@
 package com.example.pc_management_app.entity;
 
-import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import com.example.pc_management_app.entity.id.PcSoftId;
+import jakarta.persistence.UniqueConstraint;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "pc_soft")
+@Table(name = "pc_software",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_pc_software_pc_id_software_id",
+                columnNames = {"pc_id", "software_id"}))
 @Data
-public class PcSoft{
-	@EmbeddedId
-    private PcSoftId id;
+public class PcSoft {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "pc_id")
+    private Long pcId;
+
+    @Column(name = "software_id")
+    private Long softId;
 }
